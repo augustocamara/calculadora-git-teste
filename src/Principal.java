13b618c0c83700628bc4fede8b29ed230b8c4eaa
augusto <augustocamara4@gmail.com.br>
2025-05-08 15:42:30 -0300

    acrescentando tabuada

--- src/Principal.java
+++ src/Principal.java
@@ -13,6 +13,30 @@ public class Principal {
         System.out.println(num6);
         int num7 = num1 % num2;
         System.out.println(num7);
+        //fazer uma tabuada onde o usuario escolhe qual tabuada ele quer
+        int tabuada = 5;
+        int resultado = tabuada * 1;
+        System.out.println(tabuada + " x " + 1 + " = " + resultado);
+        int resultado2 = tabuada * 2;
+        System.out.println(tabuada + " x " + 2 + " = " + resultado2);
+        int resultado3 = tabuada * 3;
+        System.out.println(tabuada + " x " + 3 + " = " + resultado3);
+        int resultado4 = tabuada * 4;
+        System.out.println(tabuada + " x " + 4 + " = " + resultado4);
+        int resultado5 = tabuada * 5;
+        System.out.println(tabuada + " x " + 5 + " = " + resultado5);
+        int resultado6 = tabuada * 6;
+        System.out.println(tabuada + " x " + 6 + " = " + resultado6);
+        int resultado7 = tabuada * 7;
+        System.out.println(tabuada + " x " + 7 + " = " + resultado7);
+        int resultado8 = tabuada * 8;
+        System.out.println(tabuada + " x " + 8 + " = " + resultado8);
+        int resultado9 = tabuada * 9;
+        System.out.println(tabuada + " x " + 9 + " = " + resultado9);
+        int resultado10 = tabuada * 10;
+        System.out.println(tabuada + " x " + 10 + " = " + resultado10);
+
+
 
     }
 }
