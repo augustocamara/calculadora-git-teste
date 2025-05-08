3f0308052784feac623bd6f31803bca33c93a5dd
augusto <augustocamara4@gmail.com.br>
2025-05-08 15:33:13 -0300

    Calculadora

---
+++ src/Principal.java
@@ -0,0 +1,18 @@
+public class Principal {
+    public static void main(String[] args) {
+        //faça uma calculadora
+        int num1 = 10;
+        int num2 = 5;
+        int num3 = num1 + num2;
+        System.out.println(num3);
+        int num4 = num1 - num2;
+        System.out.println(num4);
+        int num5 = num1 * num2;
+        System.out.println(num5);
+        int num6 = num1 / num2;
+        System.out.println(num6);
+        int num7 = num1 % num2;
+        System.out.println(num7);
+
+    }
+}
